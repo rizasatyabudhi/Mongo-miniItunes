10500c3b95ef796834b7f2410e01eb64b6831341
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-04 10:06:32 +0700

    filter query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Searches through the Artist collection
@@ -7,6 +7,34 @@ const Artist = require('../models/artist');
  * @param {integer} offset How many records to skip in the result set
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ * like this: {all:[artists],count:count,offset:offset,limit:limit}
  */
+
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  // const query = Artist.find(buildQuery(criteria))
+  const query = Artist.find(buildQuery(criteria))
+    // take the sortProperty param as the key of the object
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()]).then(results => {
+    return {
+      all: results[0],
+      count: results[1],
+      offset: offset,
+      limit: limit
+    };
+  });
+};
+
+const buildQuery = criteria => {
+  const query = {};
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+  return query;
 };
