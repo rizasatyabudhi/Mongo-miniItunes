00f188a02781ab05b1bb0173de6153e967bfdb8f
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-04 13:36:40 +0700

    fixes count

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -18,7 +18,10 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
     .skip(offset)
     .limit(limit);
 
-  return Promise.all([query, Artist.count()]).then(results => {
+  return Promise.all([
+    query,
+    Artist.find(buildQuery(criteria)).count()
+  ]).then(results => {
     return {
       all: results[0],
       count: results[1],
